0695ef5ea042fc9a7132adfbf6272e2cd8c23008
SMMDR96 <sachhyammanandhar9@gmail.com>
2024-04-19 21:20:11 +0545

    Initial commit

---
+++ src/HelloGoodbye.java
@@ -0,0 +1,7 @@
+public class HelloGoodbye {
+    public static void main (String[] args){
+        System.out.print("Hello World\n");
+        System.out.print("This is really good\n");
+        System.out.println("This is after using ln");
+    }
+}
